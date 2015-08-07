0489404e5ac221454afd1548e2d433f7eee274f0
chenhuabao <chenhuabao@admindemac-mini.local>
2015-08-07 10:51:26 +0800

    添加自定义控件文件

---
+++ CHBCustomSwitchDemo/CHBCustomSwitchDemo/CustomSwitch.h
@@ -0,0 +1,13 @@
+//
+//  CustomSwitch.h
+//  CHBCustomSwitchDemo
+//
+//  Created by chenhuabao on 15/8/7.
+//  Copyright © 2015年 Tonychb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomSwitch : UIView
+
+@end
---
+++ CHBCustomSwitchDemo/CHBCustomSwitchDemo/CustomSwitch.m
@@ -0,0 +1,21 @@
+//
+//  CustomSwitch.m
+//  CHBCustomSwitchDemo
+//
+//  Created by chenhuabao on 15/8/7.
+//  Copyright © 2015年 Tonychb. All rights reserved.
+//
+
+#import "CustomSwitch.h"
+
+@implementation CustomSwitch
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
